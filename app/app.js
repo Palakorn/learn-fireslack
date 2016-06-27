46677a3264cb130cf07563ae108fe5b419620b3a
Palakorn <palakorn_plo@truecorp.co.th>
2016-06-27 12:48:43 +0700

    complete learn Cauthen

--- app/app.js
+++ app/app.js
@@ -22,13 +22,68 @@ angular
       })
       .state('login', {
         url: '/login',
-        templateUrl: 'auth/login.html'
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'auth/login.html',
+          resolve: {
+              requireNoAuth: function($state, Auth){
+                  return Auth.$requireAuth().then(function(auth){
+                      $state.go('hello');
+                  }, function(error){
+                      return;
+                  });
+              }
+          }
       })
       .state('register', {
         url: '/register',
-        templateUrl: 'auth/register.html'
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'auth/register.html',
+          resolve: {
+              requireNoAuth: function($state, Auth){
+                  return Auth.$requireAuth().then(function(auth){
+                      $state.go('hello');
+                  }, function(error){
+                      return;
+                  });
+              }
+          }
+      })
+      .state('profile', {
+          url: '/profile',
+          controller: 'ProfileCtrl as profileCtrl',
+          templateUrl: 'users/profile.html',
+          resolve: {
+              auth: function($state, Users, Auth){
+                  return Auth.$requireAuth().catch(function(){
+                      $state.go('home');
+                  });
+              },
+              profile: function(Users, Auth){
+                  return Auth.$requireAuth().then(function(auth){
+                      return Users.getProfile(auth.uid).$loaded();
+                  });
+              }
+          }
+
+      })
+      .state('hello', {
+          url: '/hello',
+          controller: 'ProfileCtrl as profileCtrl',
+          templateUrl: 'users/hello.html',
+          resolve: {
+              auth: function($state, Users, Auth){
+                  return Auth.$requireAuth().catch(function(){
+                      $state.go('home');
+                  });
+              },
+              profile: function(Users, Auth){
+                  return Auth.$requireAuth().then(function(auth){
+                      return Users.getProfile(auth.uid).$loaded();
+                  });
+              }
+          }
       });
 
     $urlRouterProvider.otherwise('/');
   })
-  .constant('FirebaseUrl', 'https://slack.firebaseio.com/');
+  .constant('FirebaseUrl', 'https://learn-authen.firebaseio.com/');
